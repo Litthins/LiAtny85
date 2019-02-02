59e0b7b8212a68f5194d72b83874f09e66d35190
Litthins <39720114+Litthins@users.noreply.github.com>
2019-02-02 19:01:18 +0800

    HAL Updates
    HAL Updates,eeprom and timercounter0.

---
+++ HAL/Sources/My_EEPROM.c
@@ -0,0 +1,38 @@
+/*
+ * My_EEPROM.c
+ *
+ * Created: 2018/12/20 18:17:11
+ *  Author: Litthins
+ */ 
+
+#include <avr/io.h>
+#include "../Headers/My_EEPROM.h"
+
+void My_EEPROM_Write(unsigned char ucAdderss, unsigned char ucData)
+{
+	//Wait last write//
+	while(EECR & (1<<EEPE));
+	//Set write mode//
+	EECR = (0<<EEPM1)|(0<<EEPM0);
+	//Set address and data//
+	EEARL = ucAdderss;
+	EEDR = ucData;
+	//Enable EEPE//
+	EECR = EECR | (1<<EEMPE);
+	//Perform write operation//
+	EECR = EECR | (1<<EEPE);
+}
+
+unsigned char My_EEPROM_Read(unsigned int ucAdderss)
+{
+	//Wait last write//
+	while(EECR & (1<<EEPE));
+	//Set write mode//
+	EECR = (0<<EEPM1)|(0<<EEPM0);
+	//Set address//
+	EEAR = ucAdderss;
+	//Enable EEPE//
+	EECR = EECR | (1<<EERE);
+	//Perform read operation//
+	return EEDR;
+}
---
+++ HAL/Sources/My_TimerCounter0.c
@@ -0,0 +1,49 @@
+/*
+ * My_TimerCounter0.c
+ *
+ * Created: 2018/12/21 10:43:53
+ *  Author: Litthins
+ */ 
+
+#include <avr/io.h>
+#include "../Headers/My_TimerCounter0.h"
+
+/* Waveform Generation Mode:Normal;
+ * Compare Output Mode:COM0B,Toggle;
+ * Clock Select:clk(io),No prescaling;
+ */
+void My_TimerCounter0_Normal_Toggle()
+{
+	PORTB=(1<<PB1);
+	DDRB=(1<<DDB1);
+	
+	GTCCR=(1<<TSM);
+	GTCCR=(1<<PSR0);
+	
+	OCR0B=0XDD;
+	
+	TCCR0A=(0<<COM0A1)|(0<<COM0A0)|(0<<COM0B1)|(1<<COM0B0)|(0<<WGM01)|(0<<WGM00);
+	TCCR0B=(0<<WGM02)|(0<<CS02)|(0<<CS01)|(1<<CS00);
+	
+	GTCCR=(0<<TSM);
+}
+
+/* Waveform Generation Mode:CTC;
+ * Compare Output Mode:COM0A,Toggle;
+ * Clock Select:clk(io),No prescaling;
+ */
+void My_TimerCounter0_CTC_Toggle()
+{
+	PORTB=(1<<PB0);
+	DDRB=(1<<DDB0);
+	
+	GTCCR=(1<<TSM);
+	GTCCR=(1<<PSR0);
+	
+	OCR0A=0X00;//Check The clk(io) Frequency.
+	
+	TCCR0A=(0<<COM0A1)|(1<<COM0A0)|(0<<COM0B1)|(0<<COM0B0)|(1<<WGM01)|(0<<WGM00);
+	TCCR0B=(0<<WGM02)|(0<<CS02)|(0<<CS01)|(1<<CS00);
+	
+	GTCCR=(0<<TSM);
+}
